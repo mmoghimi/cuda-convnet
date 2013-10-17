8889e4a34e31d8b1332595a2029095b719b0bee5
mmoghimi <mmoghimi@measure1.(none)>
2013-10-16 17:33:00 -0700

    Setting up the cudaconv on Rajib's computer.

--- aws_build.sh
+++ aws_build.sh
@@ -14,26 +14,21 @@
 # (it'll almost certainly be under /usr)
 
 # CUDA toolkit installation directory.
-#export CUDA_INSTALL_PATH=/usr/local/cuda-5.5
-export CUDA_INSTALL_PATH=/home/mmoghimi/cuda-5.5
+export CUDA_INSTALL_PATH=/usr/local/cuda
 
 # CUDA SDK installation directory.
 #export CUDA_SDK_PATH=/home/spoon/NVIDIA_GPU_Computing_SDK
-#export CUDA_SDK_PATH=/home/ubuntu/NVIDIA_CUDA-5.5_Samples/
-export CUDA_SDK_PATH=/home/mmoghimi/NVIDIA_CUDA-5.5_Samples/
-#export CUDA_SDK_PATH=/usr/
+export CUDA_SDK_PATH=/home/ubuntu/NVIDIA_CUDA-5.5_Samples/
 
 # Python include directory. This should contain the file Python.h, among others.
-export PYTHON_INCLUDE_PATH=/usr/include/python2.6
+export PYTHON_INCLUDE_PATH=/usr/include/python2.7
 
 # Numpy include directory. This should contain the file arrayobject.h, among others.
 #export NUMPY_INCLUDE_PATH=/usr/lib/pymodules/python2.7/numpy/core/include/numpy
-#export NUMPY_INCLUDE_PATH=/usr/lib/python2.6/dist-packages/numpy/core/include/numpy
-export NUMPY_INCLUDE_PATH=/usr/include/numpy
+export NUMPY_INCLUDE_PATH=/usr/lib/python2.7/dist-packages/numpy/core/include/numpy
 
 # ATLAS library directory. This should contain the file libcblas.so, among others.
-#export ATLAS_LIB_PATH=/usr/lib/atlas-base
-export ATLAS_LIB_PATH=/usr/lib64/atlas-sse3
+export ATLAS_LIB_PATH=/usr/lib/atlas-base
 
 make $*
 
